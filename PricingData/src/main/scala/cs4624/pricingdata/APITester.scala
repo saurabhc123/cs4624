0b9d0c0eae8a46300e53f7d9788dd470c9f44312
Joey Watts <joey.watts.96@gmail.com>
2017-02-08 22:51:54 -0500

    Added CSV reading/writing for EOD stock quotes.

--- PricingData/src/main/scala/cs4624/pricingdata/APITester.scala
+++ PricingData/src/main/scala/cs4624/pricingdata/APITester.scala
@@ -4,7 +4,7 @@ import com.github.nscala_time.time.Imports._
 import play.api.libs.ws.WSClient
 import play.api.libs.ws.ning.NingWSClient
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
@@ -15,11 +15,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object APITester extends App {
   val ws: WSClient = NingWSClient()
   val api = new YahooFinanceAPI(ws)
-  val future = api.getQuotes(symbol = "AAPL", startDate = new LocalDate(2017, 1, 1), endDate = LocalDate.yesterday())
+
+  val symbols = Seq("AAPL", "FB", "GILD", "KNDI", "MNKD", "NQ", "PLUG", "QQQ", "SPY", "TSLA", "VRNG")
+  val future = Future.sequence(symbols.map { sym =>
+    api.getQuotes(symbol = sym, startDate = new LocalDate(2014, 1, 1), endDate = new LocalDate(2014, 12, 31))
+  })
+
+  // Await the results from the future, write to a CSV, cleanup http client.
   try {
     import scala.concurrent.duration._
-    Await.result(future, Duration.Inf) foreach println
+    val lines = Await.result(future, Duration.Inf).flatten.map(_.toCsv)
+    import java.io._
+    val pw = new PrintWriter(new File("2014quotes.csv"))
+    // print a header.
+    pw.println("Symbol,Date,Open,High,Low,Close,Volume,AdjustedClose")
+    lines.foreach(pw.println)
+    pw.close()
   } finally {
     ws.close()
   }
+
+  // Read all the quotes from the CSV and print them out.
+  import scala.io.Source
+  val quotes = EndOfDayStockQuotes.fromCsv(Source.fromFile("2014quotes.csv").getLines.drop(1))
+  quotes.foreach(println)
 }
--- PricingData/src/main/scala/cs4624/pricingdata/EndOfDayStockQuote.scala
+++ PricingData/src/main/scala/cs4624/pricingdata/EndOfDayStockQuote.scala
@@ -12,4 +12,27 @@ case class EndOfDayStockQuote(symbol: String,
                               low: Double,
                               close: Double,
                               volume: Long,
-                              adjustedClose: Double)
+                              adjustedClose: Double) {
+  def toCsv: String = s"$symbol,$date,$open,$high,$low,$close,$volume,$adjustedClose"
+}
+object EndOfDayStockQuote {
+  def fromCsv(row: String): EndOfDayStockQuote = {
+    val columns = row.split(",")
+    EndOfDayStockQuote(
+      symbol = columns(0),
+      date = LocalDate.parse(columns(1)),
+      open = columns(2).toDouble,
+      high = columns(3).toDouble,
+      low = columns(4).toDouble,
+      close = columns(5).toDouble,
+      volume = columns(6).toLong,
+      adjustedClose = columns(7).toDouble
+    )
+  }
+}
+
+object EndOfDayStockQuotes {
+  def fromCsv(lines: TraversableOnce[String]): Seq[EndOfDayStockQuote] = {
+    lines.map(EndOfDayStockQuote.fromCsv).toSeq
+  }
+}
