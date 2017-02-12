c2a92f2f87057c652c3baf560e8139510302da5f
Joey Watts <joey.watts.96@gmail.com>
2017-02-12 14:12:08 -0500

    fix date code in hbase reading test

--- PricingData/src/main/scala/cs4624/pricingdata/HBaseReadTester.scala
+++ PricingData/src/main/scala/cs4624/pricingdata/HBaseReadTester.scala
@@ -1,7 +1,7 @@
 package cs4624.pricingdata
 
 import org.apache.hadoop.hbase.client.ConnectionFactory
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, DateTimeZone, LocalDate}
 
 /**
   * Created by joeywatts on 2/12/17.
@@ -9,7 +9,9 @@ import org.joda.time.DateTime
 object HBaseReadTester extends App {
 
   implicit val connection = ConnectionFactory.createConnection()
-  val prices = StockPrices.query("AAPL", new DateTime(2014, 1, 1).toInstant, new DateTime(2014, 12, 31).toInstant)
+  val prices = StockPrices.query("AAPL",
+    new LocalDate(2014, 1, 1).toDateTimeAtStartOfDay(DateTimeZone.UTC).toInstant,
+    new LocalDate(2014, 12, 31).toDateTimeAtStartOfDay(DateTimeZone.UTC).toInstant)
   prices.foreach(println)
 
 }
