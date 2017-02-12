df7c200c88b01b3bcc855a2058b0ac21d1b21116
Eric Williamson <ericrw96@vt.edu>
2017-02-12 13:17:47 -0500

    add more writes to the test

--- PredictionEngine/src/main/scala/main/Hbase/StockTweetWriterTest.scala
+++ PredictionEngine/src/main/scala/main/Hbase/StockTweetWriterTest.scala
@@ -2,7 +2,7 @@ package main.Hbase
 
 import java.time.Instant
 
-import main.StockTweet
+import main.{Sentiment, StockTweet}
 
 /**
   * Created by Eric on 2/12/2017.
@@ -11,5 +11,7 @@ object StockTweetWriterTest {
   def doTest(): Unit ={
     val st = StockTweet("id1", "text", "j1", Instant.now)
     StockTweetWriter.write(st)
+    val st2 = StockTweet("id2", "text", "j1", Instant.now, sentiment = Some(Sentiment.POSITIVE))
+    StockTweetWriter.write(st2)
   }
 }
