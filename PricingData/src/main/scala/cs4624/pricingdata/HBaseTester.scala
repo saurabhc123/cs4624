e3d9346a6a8e6cec0566a7e223fe0631c1e82ae2
Joey Watts <joey.watts.96@gmail.com>
2017-02-12 12:45:37 -0500

    write stock prices to HBase

--- PricingData/src/main/scala/cs4624/pricingdata/HBaseTester.scala
+++ PricingData/src/main/scala/cs4624/pricingdata/HBaseTester.scala
@@ -1,14 +1,11 @@
 package cs4624.pricingdata
 
-import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client.ConnectionFactory
 
 /**
   * Created by joeywatts on 2/12/17.
   */
 object HBaseTester extends App {
-  val conf = HBaseConfiguration.create()
-  conf.set("hbase.zookeeper.quorum", "")
   val connection = ConnectionFactory.createConnection()
 
   // Read all the quotes from the CSV and print them out.
@@ -16,5 +13,6 @@ object HBaseTester extends App {
   val quotes = EndOfDayStockQuotes.fromCsv(Source.fromFile("2014quotes.csv").getLines.drop(1))
   //quotes.foreach(println)
   val prices = quotes.flatMap(q => q.openStockPrice :: q.closeStockPrice :: Nil)
-  prices.foreach(_.write)
+  import StockPrices._
+  prices.writeAll
 }
--- PricingData/src/main/scala/cs4624/pricingdata/StockPrice.scala
+++ PricingData/src/main/scala/cs4624/pricingdata/StockPrice.scala
@@ -11,7 +11,7 @@ import org.joda.time.{DateTime, Instant}
 case class StockPrice(symbol: String, time: Instant, price: Double) {
   def write(implicit connection: Connection): Unit = {
     val table = connection.getTable(StockPrices.tableName)
-    val put = new Put(Bytes.toBytes(s"$symbol"))
+    val put = new Put(Bytes.toBytes(symbol))
     put.addColumn(Bytes.toBytes("price"), Bytes.toBytes(""), time.getMillis, Bytes.toBytes(price))
     table.put(put)
     table.close()
@@ -22,10 +22,23 @@ object StockPrices {
 
   val tableName: TableName = TableName.valueOf(Bytes.toBytes("cs4624:tweet_stock_trading:prices"))
 
-  implicit class RichTraversable(val trav: TraversableOnce[StockPrice]) {
+  implicit class RichTraversableOnce(val trav: TraversableOnce[StockPrice]) {
     def getPrice(symbol: String, instant: Instant): Option[Double] = {
       val filtered = trav.filter(price => price.symbol == symbol && price.time.isBefore(instant))
       if (filtered.isEmpty) None else Some(filtered.maxBy(_.time.getMillis).price)
+    }
+  }
+
+  implicit class RichTraversable(val trav: Traversable[StockPrice]) {
+    def writeAll(implicit connection: Connection): Unit = {
+      trav.groupBy(_.symbol).foreach { case (symbol, prices) =>
+        val table = connection.getTable(tableName)
+        val put = prices.foldLeft(new Put(Bytes.toBytes(symbol))) { case (put, price) =>
+          put.addColumn(Bytes.toBytes("price"), Bytes.toBytes(""), price.time.getMillis, Bytes.toBytes(price.price))
+        }
+        table.put(put)
+        table.close()
+      }
     }
   }
 }
